0466e7d8438c482349d40b6d218544f49e2bac48
Piuli <waseefa1@yahoo.com>
2021-09-19 00:07:18 -0400

    wip: Fix url to scrape data from

--- rankings/rankings/settings.py
+++ rankings/rankings/settings.py
@@ -87,3 +87,5 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+DOWNLOAD_DELAY = 3
--- rankings/rankings/spiders/rankings_spider.py
+++ rankings/rankings/spiders/rankings_spider.py
@@ -16,17 +16,38 @@ class RankingsSpider(scrapy.Spider):
     name = 'rankings'
 
     def start_requests(self):
-        urls = ['https://maplestory.nexon.net/rankings/job-ranking/shade/shade?rebootIndex=0']
+        page_response = requests.get('https://maplestory.nexon.net/rankings/job-ranking/shade/shade?rebootIndex=0')
+        url = page_response.url
+        urls = [url]
         
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
     
     def parse(self, response):
         name = response.css('div.c-rank-list__table-cell-text').extract()
+        rankings = response.css('h1.title').extract()
+        location = response.css('div.c-filter__item.c-filter__item--north-america.active').extract()
+        
+        page_response = requests.get('https://maplestory.nexon.net/rankings/job-ranking/shade/shade?rebootIndex=0')
+        url = page_response.url
+        
+        PATH = "C:\Program Files (x86)\chromedriver.exe"
+        driver = webdriver.Chrome(PATH)
+        driver.get(url)
+     
         yield {
-            'name': name
+            'name': name,
+            'rankings': rankings,
+            'url': url,
+            'location': location
             }
 
+
+    
+    # driver.get('https://maplestory.nexon.net/rankings/job-ranking/shade/shade?rebootIndex=0')
+    
+    
+    # print(response.url)
 
     # URL = 'https://maplestory.nexon.net/rankings/job-ranking/shade/shade?rebootIndex=0'
 

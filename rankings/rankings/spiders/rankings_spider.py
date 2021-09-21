a0ad3fe0b657e7a393b2e84042a65f67dd0a791c
Piuli <waseefa1@yahoo.com>
2021-09-20 21:48:14 -0400

    Choose shade class option

--- rankings/rankings/spiders/rankings_spider.py
+++ rankings/rankings/spiders/rankings_spider.py
@@ -51,17 +51,15 @@ class RankingsSpider(scrapy.Spider):
         
 class Rankings():
     
-    def navigate_to_rankings(self):
+    def main_site(self):  
         try:
             link = driver.find_element_by_link_text('Go to Main Site')
             link.click()
             time.sleep(2)
-        
-            # url = driver.page_source
-            # soup = BeautifulSoup(url, 'html.parser')
         except Exception:
             driver.quit()
-            
+    
+    def hover_community(self):
         try:
             community_hover = driver.find_element_by_xpath('/html/body/header/div[1]/nav/ul[1]/li[4]')
             hover = ActionChains(driver).move_to_element(community_hover)
@@ -75,17 +73,16 @@ class Rankings():
             # soup = BeautifulSoup(url, 'html.parser')
         except Exception:
             print("Can't find button\n")
-            
+    
+    def click_rankings(self):
         try:
             rankings = driver.find_element_by_class_name('ga-community-playerrankings ')
             rankings.click()
-            time.sleep(3)
-        
-            # url = driver.page_source
-            # soup = BeautifulSoup(url, 'html.parser')  
+            time.sleep(2) 
         except Exception:
             print('Can\'t find rankings button')
-            
+                 
+    def click_both(self):
         try: 
             both = driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/main/div/div/div/div[2]/div[2]/div[3]')
             both.click()
@@ -93,29 +90,65 @@ class Rankings():
         except Exception:
             print('Can\'t find both button')
 
-        url = driver.page_source
-        soup = BeautifulSoup(url, 'html.parser')  
-        
-        name = soup.find('div', class_='c-rank-list__table-row c-rank-list__table-row--header')[2]
-        print(name)
+    def change_ranking_type(self):
+        try:
+            rank_type = driver.find_elements_by_class_name('c-filter-dd__current-filtered-item ')[0]
+            rank_type.click()
+            time.sleep(1)
+        except Exception:
+            print('No rank type found')
             
+    def click_job(self):
+        try:
+            job = driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/main/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[5]')  # fix xpath
+            job.click()
+            time.sleep(1)          
+        except Exception:
+            print('No category job found')
+            
+    def click_class_type(self):
+        try:
+            class_menu = driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/main/div/div/div/div[2]/div[1]/div[1]/div/div[1]')  
+            class_menu.click()
+            time.sleep(1)
+        except Exception:
+            print('No class dropdown found')
+            
+    def click_shade(self):
+        try:
+            shade = driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/main/div/div/div/div[2]/div[1]/div[1]/div/div[2]/div[17]')
+            shade.click()
+            time.sleep(1)
+        except Exception:
+            print('Shade option not found')
+            
+    def get_ign(self):
+        url = driver.page_source
+        soup = BeautifulSoup(url, 'html.parser')
+        for ign in soup.find_all('div', class_='c-rank-list__table-row'):
+            name = ign.find('div', class_='c-rank-list__table-cell-text').text
+            print(name)
 
 if __name__ == '__main__':
     PATH = "C:\Program Files (x86)\chromedriver.exe"
     driver = webdriver.Chrome(PATH)
     driver.maximize_window()
 
-    # url = input('Type in the url: ')
-    r = Rankings()
-    
     url = 'https://maplestory.nexon.net/landing'
     driver.get(url)
     
-    page = requests.get(url)       
-    soup = BeautifulSoup(page.content, 'html.parser')
+    # page = requests.get(url)       
+    # soup = BeautifulSoup(page.content, 'html.parser')
 
-    r.navigate_to_rankings()
-    
+    r = Rankings()
+    r.main_site()
+    r.hover_community()
+    r.click_rankings()
+    r.change_ranking_type()
+    r.click_job()
+    r.click_class_type()
+    r.click_shade()
+    r.click_both()
 
 
 
